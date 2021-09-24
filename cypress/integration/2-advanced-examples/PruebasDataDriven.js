14ecfe70f5c91d4bc4b1b0aea47f02a030f9811f
DESKTOP-2TNU9RP\Admin <julieta.lopez@darwoft.com.ar>
2021-09-24 16:50:22 -0300

    formularioNombreApellidoEmailGenderMobileDatepicker

--- cypress/integration/2-advanced-examples/PruebasDataDriven.js
+++ cypress/integration/2-advanced-examples/PruebasDataDriven.js
@@ -18,13 +18,20 @@ describe('Segundo conjunto de casos de pruebas avanzadas', function(){
         cy.get('#firstName').type(this.datos.name)
         cy.get('#lastName').type(this.datos.last_name)
         cy.get('#userEmail').type(this.datos.email)
-       
-       
-        cy.get('input[name="gender"][value="Female"]').check({force: true})
-       
+        cy.get('input[name="gender"][value="'+ this.datos.gender +'"]').check({force: true}).should('be.checked')
         cy.get('#userNumber').type(this.datos.mobile)
-        cy.get('#dateOfBirthInput').type(this.datos.subjects)
+
+        //combo de datapicker, es decir seleccionar una fecha 
+        cy.get('#dateOfBirthInput').click()
+        cy.get('.react-datepicker__month-select').should('be.visible').select(this.datos.date[0])
+        cy.get('.react-datepicker__year-select').should('be.visible').select(this.datos.date[1])
+        cy.get('.react-datepicker__day--0'+ this.datos.date[2]).should('be.visible').click()
+        cy.get('#dateOfBirthInput')
+            .should('contain.value', this.datos.date[0].substring(0,3))
+            .should('contain.value', this.datos.date[1])
+            .should('contain.value', this.datos.date[2])
         cy.get('#currentAddress').type(this.datos.current_address)
+    
     })
 })
 
